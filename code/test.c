216e5aa9503906f4a263eff1fd80667f851e020b
Max Ushakov <ushmax@gmail.com>
2008-10-27 19:37:06 +0300

    first version -- testing serial

---
+++ code/test.c
@@ -0,0 +1,13 @@
+#include "uart.h"
+
+main() {
+    uart_init();
+    uart_set_baud_rate(115200);
+    while(1) {
+	for (int i = 0; i < 80; ++i) {
+	    uart_write_byte('+');
+	}
+	uart_write_byte('\r');
+	uart_write_byte('\n');
+    }
+}
