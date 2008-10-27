00f953a544997b698a8537d709aa8d76200ffda8
Max Ushakov <ushmax@gmail.com>
2008-10-27 22:39:40 +0300

    first try ad ADC

--- code/test.c
+++ code/test.c
@@ -1,13 +1,28 @@
+#include <avr/io.h>
+
 #include "uart.h"
+
+void printout(int d) {
+    int t = 1000;
+    while (t != 0) {
+	uart_write_byte(d / t + '0');
+	d %= t;
+	t /= 10;
+    }
+    uart_write_byte('\r');
+    uart_write_byte('\n');
+}
 
 main() {
     uart_init();
     uart_set_baud_rate(115200);
+    ADMUX = 0xc7; // 1100 0111
+    ADCSRA = 0x87;  // 1000 0111
     while(1) {
-	for (int i = 0; i < 80; ++i) {
-	    uart_write_byte('+');
-	}
-	uart_write_byte('\r');
-	uart_write_byte('\n');
+	ADCSRA |= 0x40;
+	while (ADCSRA & 0x40) ;
+	int t = ADCH << 8;
+	t |= ADCL;
+	printout(t);
     }
 }
