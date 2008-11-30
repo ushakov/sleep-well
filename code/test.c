841aec24c35e481e043e752befe57c20cc52c9a8
Max Ushakov <ushmax@gmail.com>
2008-11-30 23:57:36 +0300

    start with attiny44

--- code/test.c
+++ code/test.c
@@ -1,33 +1,43 @@
 #include <avr/io.h>
+#include "delay.h"
 
-#include "uart.h"
+#define DDR_LED DDRA
+#define PORT_LED PORTA
+#define BIT_LED PORTA1
 
-void printout(int d) {
-    int t = 1000;
-    while (t != 0) {
-	uart_write_byte(d / t + '0');
-	d %= t;
-	t /= 10;
-    }
-    uart_write_byte('\r');
-    uart_write_byte('\n');
+#define DDR_TR DDRA
+#define PORT_TR PORTA
+#define BIT_TR PORTA2
+
+#define DDR_RADIO DDRA
+#define PORT_RADIO PORTA
+#define BIT_RADIO PORTA7
+
+void init() {
+    DDR_LED |= BIT_LED;
+    PORT_LED |= BIT_LED;
+
+    DDR_RADIO |= BIT_RADIO;
 }
 
-main() {
-    uart_init();
-    uart_set_baud_rate(115200);
-    DDRA = 0;
-    PORTA = 0;
+void main() {
+    init();
+    while (1) {
+	delay_ms(5);
+	PORT_RADIO |= BIT_RADIO;
+	delay_ms(5);
+	PORT_RADIO &= ~BIT_RADIO;
+    }
 //    ADMUX = 0xc1; // 1100 0001, single +ADC1
 //    ADMUX = 0xd0; // 1101 0000, diff +ADC0, -ADC1, x1
-    ADMUX = 0xc0; // 1100 0000, single +ADC0
+//    ADMUX = 0xc0; // 1100 0000, single +ADC0
 //    ADMUX = 0xc9; // 1100 1001, diff -ADC0, +ADC1, x10
-    ADCSRA = 0x87;  // 1000 0111
-    while(1) {
-	ADCSRA |= 0x40;
-	while (ADCSRA & 0x40);
-	int t = ADCL;
-	t |= ADCH  << 8;
-	printout(t);
-    }
+//    ADCSRA = 0x87;  // 1000 0111
+/*     while(1) { */
+/* 	ADCSRA |= 0x40; */
+/* 	while (ADCSRA & 0x40); */
+/* 	int t = ADCL; */
+/* 	t |= ADCH  << 8; */
+/* 	printout(t); */
+/*     } */
 }
