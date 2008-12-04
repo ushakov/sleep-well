bc74607f1ddd32b5d58348c996505a1b18f9f789
Max Ushakov <ushmax@gmail.com>
2008-12-04 22:47:08 +0300

    make receiver wait

--- code/receiver.c
+++ code/receiver.c
@@ -8,8 +8,9 @@ static uint8_t buffer[BUFLEN];
 
 void main() {
     manchester_init();
+    delay_s(1);
     for (int i = 0; i < BUFLEN; ++i) {
-//	buffer[i] = manchester_wait_bit();
+	buffer[i] = manchester_wait_bit();
     }
     uart_init();
     uart_set_baud_rate(19200);
