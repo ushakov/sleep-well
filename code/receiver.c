2608d503da724df7731f94034452139534ac6002
Max Ushakov <ushmax@gmail.com>
2008-12-04 22:48:07 +0300

    output 0/1 instead of \0 \1

--- code/receiver.c
+++ code/receiver.c
@@ -15,7 +15,7 @@ void main() {
     uart_init();
     uart_set_baud_rate(19200);
     for(int i = 0; i < BUFLEN; ++i) {
-	uart_write_byte(buffer[i]);
+	uart_write_byte('0'+buffer[i]);
     }
     uart_write_byte('\r');
     uart_write_byte('\n');
