0dbb17e48ed671c639a6b5c022ad13328c0c386f
Max Ushakov <ushmax@gmail.com>
2008-12-05 23:50:42 +0300

    fix call order

--- code/receiver.c
+++ code/receiver.c
@@ -8,13 +8,13 @@ static uint8_t buffer[BUFLEN];
 
 void main() {
     manchester_init();
-    delay_s(1);
+    uart_init();
+    uart_set_baud_rate(19200);
     uart_write_byte('+');
+    delay_s(1);
     for (int i = 0; i < BUFLEN; ++i) {
 	buffer[i] = manchester_wait_bit();
     }
-    uart_init();
-    uart_set_baud_rate(19200);
     for(int i = 0; i < BUFLEN; ++i) {
 	uart_write_byte('0'+buffer[i]);
     }
