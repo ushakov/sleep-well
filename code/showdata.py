894d7a6fe35a051b6ca66bc86e4d93aeccefae39
Max Ushakov <ushmax@gmail.com>
2009-01-10 22:45:11 +0300

    new data viewer -- with zoom, scroll, and timestamps

--- code/showdata.py
+++ code/showdata.py
@@ -2,16 +2,87 @@
 
 import sys, pickle
 import math, pygame
+from pygame.locals import *
 
-w = 1000
-h = 500
-screen = None
-
-def Ginit():
+def Ginit(w, h):
     pygame.init()
-    global screen
-    screen = pygame.display.set_mode((w,h))
+    pygame.font.init()
+    return pygame.display.set_mode((w,h))
 
+class DataViewer:
+    def __init__(self, screen, data, timeshift):
+        self.w = screen.get_width()
+        self.h = screen.get_height()
+        self.screen = screen
+        self.data = []
+        self.times = []
+        n = 0
+        for d in data:
+            self.times += [(n, d['timestamp'])]
+            self.data += d['data']
+            n += len(d['data'])
+        self.scale = len(self.data) * 1.0 / self.w
+        self.max_scale = len(self.data) * 1.0 / self.w
+        self.timeshift = timeshift
+        self.x0 = 0
+
+        self.font = pygame.font.Font(None, 12)
+
+    def set_scale(self, scale, x):
+        if scale < .125:
+            scale = .125
+        if scale > self.max_scale:
+            scale = self.max_scale
+        if scale != self.scale:
+            sh = x * (self.scale - scale)
+            self.scale = scale
+            self.set_x0(self.x0 + sh)
+
+    def zoom_in(self, x):
+        self.set_scale(self.scale / 2.0, x)
+
+    def zoom_out(self, x):
+        self.set_scale(self.scale * 2, x)
+
+    def set_x0(self, x0):
+        if x0 < 0:
+            x0 = 0
+        if x0 + self.w * self.scale > len(self.data):
+            x0 = len(self.data) - (self.scale * self.w)
+        self.x0 = x0
+
+    def shift_view(self, dx):
+        self.set_x0(self.x0 + dx * self.scale)
+    
+    def update(self):
+        self.screen.fill((0,0,0))
+        for x in range(self.w):
+            low = int(x * self.scale + self.x0)
+            high = int((x + 1) * self.scale + self.x0)
+            if high <= low: high = low+1
+            inrange = [ (self.data[i] * self.h / 2048) for i in range(low, high)]
+            pygame.draw.line(self.screen, (0,255,0), (x,min(inrange)), (x, max(inrange)), 1)
+        min_x = -1
+        for t in self.times:
+            x = (t[0] - self.x0) / self.scale
+            if x >= 0 and x <= self.w:
+                pygame.draw.line(self.screen, (128,128,128), (x, self.h), (x, self.h-10), 1)
+                pygame.draw.line(self.screen, (64,64,64), (x, self.h-10), (x, 0), 1)
+                if x >= min_x:
+                    rt = t[1] + self.timeshift
+                    hours = (rt / 3600) % 24
+                    minutes = (rt % 3600) / 60
+                    seconds = rt % 60
+                    tstr = "%02d:%02d:%02d" % (hours, minutes, seconds)
+                    tsurf = self.font.render(tstr, 1, (255,255,255))
+                    trect = tsurf.get_rect()
+                    trect.bottomleft = (x, self.h - 10)
+                    min_x = trect.right + 24
+                    self.screen.blit(tsurf, trect)
+
+        pygame.draw.line(screen, (0,0,255), (0,self.h/2), (self.w, self.h/2), 1)
+        pygame.display.flip()
+        
 def show_data(data):
     Ginit()
     all = [ s for b in data for s in b['data'] ]
@@ -24,9 +95,38 @@ def show_data(data):
     pygame.draw.line(screen, (0,0,255), (0,h/2), (w, h/2), 1)
     pygame.display.flip()
 
+def handle_event(ev):
+    if ev.type == QUIT:
+        sys.exit()
+    if ev.type == KEYDOWN:
+        if ev.unicode == 'q':
+            sys.exit()
+    if ev.type == MOUSEBUTTONDOWN:
+        if ev.button == 4:
+            # wheel forward
+            dv.zoom_in(ev.pos[0])
+        if ev.button == 5:
+            # wheel back
+            dv.zoom_out(ev.pos[0])
+        # print "button down:", ev.pos, ev.button
+    if ev.type == MOUSEMOTION:
+        if ev.buttons[0] != 0:
+            dv.shift_view(-ev.rel[0])
+        # print "motion:", ev.pos, ev.rel, ev.buttons
+
 if __name__ == "__main__":
     data = pickle.load(open(sys.argv[1]))
     print len(data)
-    show_data(data)
+    screen = Ginit(1000, 500)
+    dv = DataViewer(screen, data, 0)
+    dragging = False
+    prev = None
+
+    dv.update()
+    clock = pygame.time.Clock()
     while True:
-        pass
+        clock.tick(60)
+        for ev in pygame.event.get():
+            handle_event(ev)
+        dv.update()
+
